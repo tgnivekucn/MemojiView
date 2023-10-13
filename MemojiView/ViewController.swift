d3bbaf2913103e36ecb1ae9c1df0bb0ab793e24a
Kuang-Yu <kuangyu.nien@somnics.com>
2023-10-13 16:01:52 +0800

    Add demo code

--- MemojiView/ViewController.swift
+++ MemojiView/ViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+    private let memojiView = MemojiView(frame: CGRect(origin: .zero, size: CGSize(width: 300, height: 300)))
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        memojiView.backgroundColor = .yellow
+        self.view.addSubview(memojiView)
+        memojiView.imageChanged = { image in
+            print("got it")
+            self.startResizeImageViewAnimation(imageView: self.memojiView)
+        }
     }
 
-
+    private func startResizeImageViewAnimation(imageView: UIImageView) {
+        DispatchQueue.main.async {
+            UIView.animate(withDuration: 3.0,
+                           delay: 0.0,
+                           options: [.curveLinear, .repeat, .autoreverse],
+                           animations: { () -> Void in
+                self.memojiView.resizeView(size: 50)
+            }, completion: { (finished: Bool) -> Void in
+                
+            })
+        }
+    }
 }
-
